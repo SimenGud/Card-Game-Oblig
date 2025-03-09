57c9a338bc13b35fb4de6d04af1b7a0029f40466
SimenGud <simeng2003@gmail.com>
2025-03-09 16:05:34 +0100

    Added suits and dealHand method

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/DeckOfCards.java
@@ -2,4 +2,10 @@ package no.ntnu.idatx2003.oblig4.cardgame;
 
 public class DeckOfCards {
 
+  private final char[] suits = { 'S', 'H', 'D', 'C' };
+
+  dealHand(int n) {
+    // Implement this method
+  }
+
 }
