d82f6971a07bea13a77689385044e79e41ca0378
SimenGud <simeng2003@gmail.com>
2025-03-10 05:07:31 +0100

    Added a example flush method and docs to GameController

--- src/main/java/no/ntnu/idatx2003/oblig4/cardgame/logic/GameController.java
+++ src/main/java/no/ntnu/idatx2003/oblig4/cardgame/logic/GameController.java
@@ -2,6 +2,7 @@ package no.ntnu.idatx2003.oblig4.cardgame.logic;
 
 import no.ntnu.idatx2003.oblig4.cardgame.models.DeckOfCards;
 import no.ntnu.idatx2003.oblig4.cardgame.models.HandOfCards;
+import no.ntnu.idatx2003.oblig4.cardgame.models.PlayingCard;
 
 public class GameController {
   private final DeckOfCards deck;
@@ -11,11 +12,42 @@ public class GameController {
     this.deck = new DeckOfCards();
   }
 
+
+  /**
+   * Deals a hand of 5 cards from the deck.
+   *
+   * @return the hand of cards
+   */
+
   public HandOfCards dealHand() {
     deck.shuffleDeck();
     lastHand = deck.dealHand(5); // Save the last dealt hand
     return lastHand;
   }
+
+  /*
+
+  //Example method for automatically dealing a flush.
+  public HandOfCards dealHand() {
+    deck.shuffleDeck();
+    HandOfCards hand = new HandOfCards();
+
+    for (int i = 0; i < 5; i++) {
+      int face = (i % 13) + 1; // Ensures values between 1 (Ace) to 13 (King)
+      hand.addCard(new PlayingCard('H', face));
+    }
+
+    lastHand = hand; // Save the last hand
+    return hand;
+  }
+
+   */
+
+  /**
+   * Returns the last hand dealt.
+   *
+   * @return the last hand dealt
+   */
 
   public HandOfCards getLastHand() {
     return lastHand;
